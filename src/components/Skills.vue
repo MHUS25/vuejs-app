3e679581d293eede8aff08134633e0bb1560f335
Muna <mmhussein125@gmail.com>
2019-03-09 20:31:35 +0000

    styling

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,10 +2,9 @@
   <div class="hello">
     <div class="holder">
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
       </ul>
-      <div v-bind:style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight }"></div>
-    </div>
+      <p>These are the skills that you possess.</p>    </div>
   </div>
 </template>
 
@@ -28,24 +27,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+.holder {
+    background: #fff;
+  }
 
-.alert {
-  background-color: pink;
-  width: 100%;
-  height: 30px
-};
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
 </style>
