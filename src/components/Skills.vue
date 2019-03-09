9e397c7f06f1d9bd8347b2b9bcc585348be65919
Muna <mmhussein125@gmail.com>
2019-03-09 19:04:34 +0000

    Playing with vue directives and templating

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,6 +5,8 @@
         <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
       </ul>
 
+      <p v-if="skills.length >= 1">You have more than 1 skill, Hooray!</p>
+      <p v-else> You have less than or equal to 1 skill</p>
     </div>
   </div>
 </template>
