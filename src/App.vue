6c1a6b5670c73f43a1323bb33908eb98e795f612
Muna <mmhussein125@gmail.com>
2019-03-10 00:39:42 +0000

    routing with vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<template>
+  <template>
   <div id="app">
-    <Skills/>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    <router-view/>
   </div>
+</template>
 </template>
 
 <script>
@@ -24,7 +29,6 @@ body {
   display: grid;
   grid-template-rows: auto;
   justify-items: center;
-  align-items: center;
 }
 body, html {
   margin: 0;
@@ -32,5 +36,28 @@ body, html {
 }
 #app {
     width: 50%;
+}
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
 }
 </style>
