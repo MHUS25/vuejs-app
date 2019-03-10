d3a02fbc515994b4efa2620b6a550a70c3ccd4d3
Muna <mmhussein125@gmail.com>
2019-03-10 00:05:22 +0000

    decreases min character form input to 2

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,7 +3,7 @@
     <div class="holder">
 
       <form @submit.prevent="addSkill">
-        <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
+        <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:2'" name="skill">
         <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
       </form>
       <ul>
