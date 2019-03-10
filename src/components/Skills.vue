9f08f27c362e04cf0f43c7ca519989b8b9700aa7
Muna <mmhussein125@gmail.com>
2019-03-10 00:50:24 +0000

    remove items functionality

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,7 +10,10 @@
       </form>
       <ul>
         <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
-        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+        <li v-for="(data, index) in skills" :key='index'>
+          {{ data.skill }}
+             <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+          </li>
         </transition-group>
       </ul>
       <p>These are the skills that you possess.</p>    </div>
@@ -39,6 +42,9 @@ export default {
             console.log('Not valid');
           }
         })
+      },
+      remove(id) {
+        this.skills.splice(id, 1);
       }
   }
 }
@@ -47,6 +53,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
 
 .holder {
     background: #fff;
@@ -93,5 +100,10 @@ export default {
     display: inline-block;
     padding: 5px;
     margin-top: -20px;
+  }
+
+  i {
+    float: right;
+    cursor: pointer;
   }
 </style>
